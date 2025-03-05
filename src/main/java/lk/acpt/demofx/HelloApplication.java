ffaeaaa8f244c77fa21bd675addc72a3b5fdd670
risinu <risinu001@gmail.com>
2025-03-05 15:21:26 +0530

    bug 02 commit 1

--- src/main/java/lk/acpt/demofx/HelloApplication.java
+++ src/main/java/lk/acpt/demofx/HelloApplication.java
@@ -13,6 +13,7 @@ public class HelloApplication extends Application {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("calculator-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Hello!");
+        System.out.println("bug02");
         stage.setScene(scene);
         stage.show();
     }
