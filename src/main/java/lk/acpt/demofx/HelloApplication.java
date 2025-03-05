d7817eba6b993e86e44d9b30a00c5f87c36512fd
risinu <risinu001@gmail.com>
2025-03-05 12:11:51 +0530

    added test out

--- src/main/java/lk/acpt/demofx/HelloApplication.java
+++ src/main/java/lk/acpt/demofx/HelloApplication.java
@@ -20,5 +20,6 @@ public class HelloApplication extends Application {
     public static void main(String[] args) {
         launch();
         System.out.println("Hello World!");
+        System.out.println("Hello World! 2");
     }
 }
